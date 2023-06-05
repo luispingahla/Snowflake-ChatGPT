72b17cc97735122aa5f7fd42771ef8cc5ca61764
luispingahla <135360351+luispingahla@users.noreply.github.com>
2023-06-05 10:38:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from flask import Flask
 from chat_gpt.chat_gpt_controller import chat_gpt_route_path, chat_gpt_route
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Create the Flask application
+app = Flask(__name__)
 
+# Register modules/blueprints
+app.register_blueprint(chat_gpt_route, url_prefix=f'/{chat_gpt_route_path}')
 
 def bootstrap():
-    # create app
-    app = Flask(__name__)
-    # register modules/blueprints
-    app.register_blueprint(chat_gpt_route, url_prefix=f'/{chat_gpt_route_path}')
-    # start app
+    # Start the Flask application
     app.run(port=3000, debug=True)
-
 
 if __name__ == '__main__':
     bootstrap()
+
