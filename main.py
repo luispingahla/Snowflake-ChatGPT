8169524b5d8653e5c650f3fe591a98267edc8602
luispingahla <135360351+luispingahla@users.noreply.github.com>
2023-06-05 10:22:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# create app
+app = Flask(__name__)
+
 def bootstrap():
-    # create app
-    app = Flask(__name__)
+
     # register modules/blueprints
     app.register_blueprint(chat_gpt_route, url_prefix=f'/{chat_gpt_route_path}')
     # start app
